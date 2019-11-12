8f86f0280e916ce423818c03aae752d9e5e623c7
Joseph Hancock <joseph.hancock@imsar.com>
2019-11-12 09:21:13 -0700

    Resolve warning

--- urjtag/bindings/python/chain.c
+++ urjtag/bindings/python/chain.c
@@ -949,7 +949,7 @@ urj_pyc_bsdl_get_path (urj_pychain_t *self)
 static PyObject *
 urj_pyc_set_jtag_data_dir (urj_pychain_t *self, PyObject *args)
 {
-    urj_chain_t *urc = self->urchain;
+    //urj_chain_t *urc = self->urchain;
     char *data_dir;
 
     if (!PyArg_ParseTuple (args, "s", &data_dir))
