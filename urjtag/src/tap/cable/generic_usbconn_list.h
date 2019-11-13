5385748f8b06667790fc106a47b6a82b4bbdbdba
Joseph Hancock <joseph.hancock@imsar.com>
2019-11-13 14:13:11 -0700

    Add DigilentHS2 billed to r75 firmware sustaining eng

--- urjtag/src/tap/cable/generic_usbconn_list.h
+++ urjtag/src/tap/cable/generic_usbconn_list.h
@@ -64,6 +64,7 @@ _URJ_USB_FTDX(usbjtagrs232)
 _URJ_USB_FTDX(usbscarab2)
 _URJ_USB_FTDX(usbtojtagif)
 _URJ_USB_FTDX(digilenths1)
+_URJ_USB_FTDX(digilenths2)
 _URJ_USB_FTDX(ft4232)
 #endif
 #ifdef ENABLE_CABLE_USBBLASTER
